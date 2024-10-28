7541506fecf07a4cea7432297ae9781b2c534180
Sophie Pope <sophiepope393@gmail.com>
2024-10-28 18:35:20 +0000

    anagram code wars ✅

--- kyu_7/anagram.py
+++ kyu_7/anagram.py
@@ -1,12 +1,14 @@
 def is_anagram(test, original):
+    # make the input all lower case, so when sorted they will match
     lower_test = test.lower()
     lower_original = original.lower()
+    # sort the letter, so if the words are anagrams they will match
     sorted_test = ''.join(sorted(lower_test))
     sorted_org = ''.join(sorted(lower_original))
     if sorted_test == sorted_org:
-        return True, f'The word {test} is an anagram of {original}'
+        return True
     else:
         return False 
     
-print(is_anagram('Buckethead', 'DeathCubeK'))
+print(is_anagram('Twoo', 'Woot'))
 
